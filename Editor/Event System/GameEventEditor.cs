2b52e0df33e69b518e7fcb8dfad4dbd44f087e22
Andrea <andrea.amato.89@gmail.com>
2020-05-12 09:18:18 +0100

    Define a simple custom inspector view for GameEvent scriptable-objects

---
+++ Editor/Event System/GameEventEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Amheklerior.Core.EventSystem;
+
+[CustomEditor(typeof(GameEvent), true)]
+public class GameEventEditor : Editor {
+    
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+        if (GUILayout.Button("Raise")) ((GameEvent) target).Raise();
+        EditorGUI.EndDisabledGroup();
+    }
+    
+}
