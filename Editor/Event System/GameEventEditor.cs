5899dfbad80c7d1e45f44ca5a65e289e94a012f9
Andrea <andrea.amato.89@gmail.com>
2020-05-24 09:12:13 +0100

    Puts GameEventEditor into proper namespace

--- Editor/Event System/GameEventEditor.cs
+++ Editor/Event System/GameEventEditor.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using Amheklerior.Core.EventSystem;
 
-[CustomEditor(typeof(GameEvent), true)]
-public class GameEventEditor : Editor {
-    
-    public override void OnInspectorGUI() {
-        DrawDefaultInspector();
+namespace Amheklerior.Core.EventSystem.Editor {
 
-        EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
-        if (GUILayout.Button("Raise")) ((GameEvent) target).Raise();
-        EditorGUI.EndDisabledGroup();
+    [CustomEditor(typeof(GameEvent), true)]
+    public class GameEventEditor : UnityEditor.Editor {
+
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+            if (GUILayout.Button("Raise")) ((GameEvent) target).Raise();
+            EditorGUI.EndDisabledGroup();
+        }
     }
-    
+
 }
