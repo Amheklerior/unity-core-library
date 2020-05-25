ab780aba1f6a835440c728f2a1fcc4a114af660b
Andrea <andrea.amato.89@gmail.com>
2020-05-25 09:07:45 +0100

    Adds an abstraction and a debuggable version of the pool

---
+++ Runtime/Object Pooling/AbstractPool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Amheklerior.Core.ObjectPooling {
+
+    public delegate T CreationFunc<T>();
+    
+    public abstract class AbstractPool<T> : IPool<T> {
+
+        public Action<T> OnGet { get; set; }
+        public Action<T> OnPut { get; set; }
+
+        public AbstractPool(CreationFunc<T> create, int capacity = 100, bool canExpand = false) {
+            if (capacity <= 0)
+                throw new ArgumentException($"The pool capacity must be greater than zero. Was {capacity} instead");
+
+            _canExpand = canExpand;
+            _create = create;
+            Fill(capacity);
+        }
+
+        public T Get() {
+            if (IsEmpty && _canExpand) Expand();
+            if (IsEmpty) return default;
+
+            T instance = GetFromPool();
+            OnGet?.Invoke(instance);
+            return instance;
+        }
+
+        public void Put(T instance) {
+            if (!IsManaged(instance)) return;
+
+            OnPut?.Invoke(instance);
+            PutBackIntoPool(instance);
+        }
+
+        
+        #region Internals 
+
+        private readonly CreationFunc<T> _create;
+        private readonly bool _canExpand;
+
+        protected virtual void Fill(int howMany) {
+            T newInstance;
+            for (int i = 0; i < howMany; i++) {
+                newInstance = _create();
+                AddToPool(newInstance);
+            }
+        }
+
+        protected virtual void Expand() => Fill(Capacity);
+
+        protected virtual int Capacity { get; }
+        protected virtual bool IsEmpty { get; }
+        protected abstract bool IsManaged(T instance);
+        protected abstract void AddToPool(T newInstance);
+        protected abstract T GetFromPool();
+        protected abstract void PutBackIntoPool(T instance);
+
+        #endregion
+
+    }
+
+}
--- Runtime/Object Pooling/Pool.cs
+++ Runtime/Object Pooling/Pool.cs
@@ -1,98 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Amheklerior.Core.ObjectPooling {
+    
+    public class Pool<T> : AbstractPool<T> {
 
-    public delegate T CreationFunc<T>();
-
-    /**
-     * GOAL: Maximize operational speed performances
-     * 
-     * With fixed capacity: 
-     *      Get -> O(1) 
-     *      Release -> O(1)
-     * 
-     * Expandable:
-     *      Get -> O(1), with worse-case scenario of O(n)
-     *      Release -> O(1)
-     *      
-     * Memory footprint:
-     *      An HashSet to track all instances managed by the pool
-     *      A Queue to track all the ready-to-use instances
-     * 
-     * 
-     * How to use: 
-     *      At initialization the pool will be fill at full capacity.
-     *          By default, the pool has fixed capacity of 100 slots. 
-     *          You can specify a different capacity and/or its ability to expand upon needs in the constructor only.
-     *      Use get to get and use an instance from the pool
-     *      Use put to put it back to the pool for future usage
-     *      
-     *      
-     * If you want to know more about Object Pooling in general, [Game Programming Patterns] is a brilliant source that covers the topic.
-     */
-    public class Pool<T> : IPool<T> {
-        
-        // SHOULD THEY BE SET ONLY?
-        public Action<T> OnGet { get; set; }
-        public Action<T> OnPut { get; set; }
-
-        public Pool(CreationFunc<T> create, int capacity = 100, bool canExpand = false) {
-            if (capacity <= 0)
-                throw new ArgumentException("Pool capacity must be greater than zero.");
-
-            _canExpand = canExpand;
-            _capacity = capacity;
-            _create = create;
-            Fill(capacity);
-        }
-
-        public T Get() {
-            if (IsEmpty && _canExpand) Expand();
-            if (IsEmpty) return default;
-
-            T instance = _availableInstances.Dequeue();
-            OnGet?.Invoke(instance);
-            return instance;
-        }
-
-        public void Put(T instance) {
-            if (!IsManaged(instance)) return; 
-
-            OnPut?.Invoke(instance);
-            _availableInstances.Enqueue(instance);
-        }
-
-
-        #region Internals 
-
-        private readonly CreationFunc<T> _create;
-        private readonly bool _canExpand;
-        
         private int _capacity;
+
         private HashSet<T> _managedInstances = new HashSet<T>();
         private Queue<T> _availableInstances = new Queue<T>();
 
-        private bool IsEmpty => _availableInstances.Count == 0;
-        private bool IsManaged(T instance) => _managedInstances.Contains(instance);
+        public Pool(CreationFunc<T> create, int capacity = 100, bool canExpand = false) : 
+            base(create, capacity, canExpand) => _capacity = capacity;
+
+        protected override int Capacity => _capacity;
+
+        protected override bool IsEmpty => _availableInstances.Count == 0;
+
+        protected override bool IsManaged(T instance) => _managedInstances.Contains(instance);
+
+        protected override T GetFromPool() => _availableInstances.Dequeue();
+
+        protected override void PutBackIntoPool(T instance) => _availableInstances.Enqueue(instance);
+
+        protected override void AddToPool(T newInstance) {
+            _managedInstances.Add(newInstance);
+            _availableInstances.Enqueue(newInstance);
+        }
         
-        private void Fill(int howMany) {
-            T newInstance;
-            for (int i = 0; i < howMany; i++) {
-                newInstance = _create();
-                _managedInstances.Add(newInstance);
-                _availableInstances.Enqueue(newInstance);
+        protected override void Expand() {
+            base.Expand();
+            _capacity *= 2;
+        }
+
+    }
+
+    
+    #region Debuggable version
+
+    public class DebuggablePool<T> : Pool<T> {
+
+        public DebuggablePool(CreationFunc<T> create, int capacity = 100, bool canExpand = false) :
+            base(create, capacity, canExpand) { }
+
+
+        protected override bool IsEmpty {
+            get {
+                var isEmpty = base.IsEmpty;
+                if (isEmpty)
+                    System.Diagnostics.Debug.Write($"The pool does not have any more instances to give.");
+                return isEmpty;
             }
         }
 
-        private void Expand() {
-            if (!IsEmpty || !_canExpand) return;
-
-            Fill(_capacity);
-            _capacity *= 2;
+        protected override bool IsManaged(T instance) {
+            var isManaged = base.IsManaged(instance);
+            System.Diagnostics.Debug.Write(
+                $"The instance {instance} {(isManaged? "is" : "is not")} a managed instance. " +
+                $"Hence, it {(isManaged ? "can " : "cannot")} be put back to the pool.");
+            return isManaged;
         }
-        
-        #endregion
-        
+
+        protected override void AddToPool(T newInstance) {
+            System.Diagnostics.Debug.Write($"The instance {newInstance} has been created and added to the pool.");
+            base.AddToPool(newInstance);
+        }
+
+        protected override void Expand() {
+            System.Diagnostics.Debug.Write($"The pool is going to expand.");
+            base.Expand();
+        }
+
+        protected override T GetFromPool() {
+            var instance = base.GetFromPool();
+            System.Diagnostics.Debug.Write($"The instance {instance} is taken from the pool.");
+            return instance;
+        }
+
+        protected override void PutBackIntoPool(T instance) {
+            System.Diagnostics.Debug.Write($"The instance {instance} has been released and put back to the pool.");
+            base.PutBackIntoPool(instance);
+        }
     }
+
+    #endregion
+    
 }
